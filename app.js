08aeb68d57e5e8768633d01971411ea7387c9584
Carlos Morales <carlos.rey.morales@gmail.com>
2022-08-28 17:26:55 -0400

    feature/enabledPublicCorsOnBackend

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -7,6 +8,8 @@ var users = require('./routes/users');
 
 var app = express();
 
+app.use(cors());
+app.options('*', cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
